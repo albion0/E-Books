db9120931c9dbbf7630359e718b832d0ac407774
lorik.tola <lt47136@ubt-uni.net>
2022-05-22 21:37:21 +0200

    contact us component added -> work to be done

--- client/src/components/ContactUs/ContactUs.js
+++ client/src/components/ContactUs/ContactUs.js
@@ -1,10 +1,18 @@
-import classes from "./ContactUs.module.css";
+import "./ContactUs.css";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs
-      <h3>Test init</h3>
-    </div>
+   <div>
+       <div className="contact-Container">
+                <form className="contact-form">
+                        <p>Contact Us</p>
+                        <input type="text"  placeholder="Name" name="name"  className="form-input" />
+                        <input type="text"  placeholder="Email" name="email" className="form-input" />
+                        <textarea name="description" id="" cols="30" rows="10" placeholder="Message"  className="form-input"></textarea>
+                        <input type="submit" name="submit" className="submit-button" value="SUBMIT"/>
+                </form>
+            </div>
+   </div>
   )
 }
 
