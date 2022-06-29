e74d1db77ab611e44a7a9ef03a14f1c77665b3ae
lorik.tola <lt47136@ubt-uni.net>
2022-06-29 12:45:49 +0200

    homepage structure updated, still some work to be done

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,29 +1,65 @@
-import Cards from "./Cards/Cards";
+//import Cards from "./Cards/Cards";
+//import classes from "./Home.module.css";
+//import bookImg from "../../assets/images/book.png";
+import "./Home.css";
+import book1 from "./image/book-1.png";
+import book2 from "./image/book-2.png";
+import book3 from "./image/book-3.png";
+import book4 from "./image/book-4.png";
+import book5 from "./image/book-5.png";
+import book6 from "./image/book-6.png";
+import stand from "./image/stand.png";
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
 
-import classes from "./Home.module.css";
-import bookImg from "../../assets/images/book.png";
+const swiper = new Swiper(".books-slider", {
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
 
-const booksData = [];
-
-for(let i = 1; i <= 5; i++) {
-  booksData.push({
-    id: 'key' + i,
-    img: bookImg,
-    title: "Book " + i,
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 
-    price: 20,
-    date: "4/15/2022"
-  })
-}
 
 const Home = () => {
   return (
-    <div className={classes.wrapper}>
-      <Cards title="New Releases" items={booksData} />
-      <Cards title="Recommended Books" items={booksData} />
-      <Cards title="Best-selling Books" items={booksData} />
-    </div>
+    <section className="home" id="home">
+      <div className="row">
+          <div className="content">
+              <h3>upto 75% off</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam deserunt nostrum accusamus. Nam alias sit necessitatibus, aliquid ex minima at!</p>
+              <a href="#" className="btn">shop now</a>
+          </div>
+
+          <div className="swiper books-slider">
+              <div className="swiper-wrapper">
+                  <a href="#" className="swiper slide"><img src={book1} alt=""/></a>
+                  <a href="#" className="swiper slide"><img src={book2} alt=""/></a>
+                  <a href="#" className="swiper slide"><img src={book3} alt=""/></a>
+                  <a href="#" className="swiper slide"><img src={book4} alt=""/></a>
+                  <a href="#" className="swiper slide"><img src={book5} alt=""/></a>
+                  <a href="#" className="swiper slide"><img src={book6} alt=""/></a>
+              </div>
+              <img src={stand} className="stand" alt=""/>
+          </div>
+      </div>
+    </section>
   )
 }
 
 export default Home
+
